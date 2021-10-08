b2825d88097c145e83e33b843b415e52d9d36e7e
Everglowzz <839914339@qq.com>
2021-10-08 17:50:02 +0800

    更新

--- lcLibry/src/main/java/com/example/lcontroller/LcUtils.kt
+++ lcLibry/src/main/java/com/example/lcontroller/LcUtils.kt
@@ -1,26 +1,39 @@
 package com.example.lcontroller
+
 import android.content.Context
 import android.content.Intent
+import android.net.Uri
+import android.text.TextUtils
+import android.util.Log
 import com.avos.avoscloud.*
 
-object  LcUtils {
-  open  fun init(context: Context,appID:String,appKey:String,objectid:String){
-      AVOSCloud.initialize(context,appID,appKey)
-        var lcQuery =  AVQuery<AVObject>("DataBean")
-      lcQuery.whereEqualTo("appid",objectid).getFirstInBackground(object :GetCallback<AVObject>(){
-          override fun done(`object`: AVObject?, e: AVException?) {
-              if (e==null) {
-                  `object`?.let {
-                      var isShow =   it.getString("isshow")
-                      if (isShow == "1"){
-                          var url = it.getString("wapurl")
-                          val intent = Intent(context, LcActivity::class.java)
-                          intent.putExtra("url", url)
-                          context.startActivity(intent)
-                      }
-                  }
-              }
-          }
-      })
+object LcUtils {
+    private var TAG = "lcUtils"
+    open fun init(context: Context, appID: String, appKey: String, objectid: String) {
+        AVOSCloud.initialize(context, appID, appKey)
+        var lcQuery = AVQuery<AVObject>("DataBean")
+        lcQuery.whereEqualTo("appid", objectid)
+            .getFirstInBackground(object : GetCallback<AVObject>() {
+                override fun done(`object`: AVObject?, e: AVException?) {
+                    if (e == null) {
+                        `object`?.let {
+                            var isShow = it.getString("isshow")
+                            Log.e(TAG, "appID:$objectid,isshow:$isShow")
+                            if (isShow == "1") {
+                                var url = it.getString("wapurl")
+                                Log.e(TAG, "url:$url")
+                                if (!TextUtils.isEmpty(url)) {
+                                    val intent = Intent()
+                                    intent.action = Intent.ACTION_VIEW
+                                    intent.data = Uri.parse(url)
+                                    context.startActivity(intent)
+                                }
+                            }
+                        }
+                    } else {
+                        e.printStackTrace()
+                    }
+                }
+            })
     }
 }
--- myapplication/src/main/java/com/example/myapplication/MainActivity.kt
+++ myapplication/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,13 +2,12 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.lcontroller.LcActivity
 import com.example.lcontroller.LcUtils
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        LcUtils.init(this,"XV1EKekyVJJEfln5vNBGk2rQ-MdYXbMMI","flgB6c1qplRYDSzT1pPixl3b","cgapp")
+        LcUtils.init(this,"XV1EKekyVJJEfln5vNBGk2rQ-MdYXbMMI","flgB6c1qplRYDSzT1pPixl3b","足球金手指")
     }
 }
